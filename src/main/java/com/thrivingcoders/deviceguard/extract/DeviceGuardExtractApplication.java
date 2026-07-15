ea9b4d6d6319b16d40cbbb2482fbe0e8a5809722
Andrej Naumov <andrejs@list.ru>
2026-07-15 22:23:13 +0200

    Initial Spring Boot project

---
+++ src/main/java/com/thrivingcoders/deviceguard/extract/DeviceGuardExtractApplication.java
@@ -0,0 +1,13 @@
+package com.thrivingcoders.deviceguard.extract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceGuardExtractApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeviceGuardExtractApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thrivingcoders/deviceguard/extract/DeviceGuardExtractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thrivingcoders.deviceguard.extract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceGuardExtractApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
