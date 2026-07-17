d6c6cda9aba46efdd4d5e7a31ef7fcda9f79a3bc
Andrej Naumov <andrejs@list.ru>
2026-07-17 16:26:54 +0200

    Add dotenv configuration support

--- src/main/java/com/thrivingcoders/deviceguard/extract/DeviceGuardExtractApplication.java
+++ src/main/java/com/thrivingcoders/deviceguard/extract/DeviceGuardExtractApplication.java
@@ -1,13 +1,41 @@
 package com.thrivingcoders.deviceguard.extract;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.MapPropertySource;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class DeviceGuardExtractApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DeviceGuardExtractApplication.class, args);
-    }
+        final Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
 
+        final Map<String, Object> dotenvProperties = dotenv.entries()
+                .stream()
+                .collect(Collectors.toMap(
+                        DotenvEntry::getKey,
+                        DotenvEntry::getValue
+                ));
+
+        final SpringApplication application =
+                new SpringApplication(DeviceGuardExtractApplication.class);
+
+        application.addInitializers(applicationContext ->
+                applicationContext.getEnvironment()
+                        .getPropertySources()
+                        .addLast(new MapPropertySource(
+                                "dotenv",
+                                dotenvProperties
+                        ))
+        );
+
+        application.run(args);
+    }
 }
